88ee222425e58132f11adbda6408a2471d291bf8
Dmitry Pisarev <dmitry.pisarev@clearscale.net>
2015-03-07 20:48:15 +0100

    Pass a chef/client/roles into coupa/role as is. We are agree to use a few roles at ones delimited by commas.

--- recipes/client.rb
+++ recipes/client.rb
@@ -35,8 +35,6 @@ ruby_block "Check ca-certificates" do
 end
 
 
-node.override[:coupa][:role] = node[:chef][:client][:roles].split(",").first.strip
-
 template "/etc/chef_coupa_attr.json" do
   source "chef_coupa_attr.json.erb"
   owner "root"
