a8caf7f266fb70f6a2adcc8034491118b8b2dc86
Dmitry Pisarev <dmitry.pisarev@clearscale.net>
2015-04-04 20:42:20 +0200

    CHEF-250 Respect s3 endpoint of a region.

--- recipes/server.rb
+++ recipes/server.rb
@@ -26,7 +26,10 @@ require 'aws-sdk'
 
 bucket_name = "#{node[:coupa][:deployment].chars.select{|x| x.match(/[a-z0-9]/)}.join}chef.#{node[:coupa][:serverdomain]}"
 
-api = AWS::S3.new(:access_key_id => node[:coupa][:s3][:access_key], :secret_access_key => node[:coupa][:s3][:secret_key])
+api = AWS::S3.new(
+  :access_key_id => node[:coupa][:s3][:access_key],
+  :secret_access_key => node[:coupa][:s3][:secret_key],
+  :region => node['ec2']['placement_availability_zone'].chop)
 unless api.buckets[bucket_name].exists?
   api.buckets.create(bucket_name)
 end
